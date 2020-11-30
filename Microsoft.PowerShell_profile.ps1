ce2775e3535cb36c088fc9149bcaf5832c65bd7c
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2020-11-30 16:51:51 +0100

    Adds Windows Terminal settings file and some git aliases to Powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,9 @@ Import-Module -Name posh-git
 Import-Module -Name Get-ChildItemColor
 
 Start-SshAgent -Quiet
+
+function Get-GitStatusAll { git status -uall }
+function Add-AllFilesToGit { git add -A }
+
+Set-Alias -Name gsa -Value Get-GitStatusAll
+Set-Alias -Name gaa -Value Add-AllFilesToGit
